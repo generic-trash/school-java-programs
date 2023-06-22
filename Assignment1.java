db9d0ee7e1683d39bf63bb99ec743c11057f683b
***** *** <rohroexperiment@gmail.com>
2023-06-22 07:33:07 +0530

    add 2 files

---
+++ Assignment1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        int lowercaseCount = 0;
+        int uppercaseCount = 0;
+        int specialCharCount = 0;
+        int digitCount = 0;
+        int wordCount = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+
+            if (Character.isLowerCase(ch)) {
+                lowercaseCount++;
+            } else if (Character.isUpperCase(ch)) {
+                uppercaseCount++;
+            } else if (Character.isDigit(ch)) {
+                digitCount++;
+            } else if (Character.isWhitespace(ch)) {
+                wordCount++;
+            } else {
+                specialCharCount++;
+            }
+        }
+
+        // Increment wordCount for the last word
+        wordCount++;
+
+        System.out.println("Lowercase letters: " + lowercaseCount);
+        System.out.println("Uppercase letters: " + uppercaseCount);
+        System.out.println("Special characters: " + specialCharCount);
+        System.out.println("Digits: " + digitCount);
+        System.out.println("Number of words: " + wordCount);
+
+        sc.close();
+    }
+}
---
+++ Assignment2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name");
+        String name = sc.nextLine();
+        int first = name.indexOf(" "), last = name.lastIndexOf(" ");
+    }
+}
